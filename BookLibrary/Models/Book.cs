15e93d73172de9efebc8873ab2dee02c5f1debca
Lithium87 <victoriavandeva@gmail.com>
2026-07-04 19:13:52 +0300

    Create Book class

---
+++ BookLibrary/Models/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookLibrary.Models
+{
+    public class Book
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public int Year { get; private set; }
+        public int Pages { get; private set; }
+        public string Isbn { get; private set; }
+
+        public Book(string title, string author, int year, int pages, string isbn)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Title is required.", nameof(title));
+            }
+            if (string.IsNullOrWhiteSpace(author))
+            {
+                throw new ArgumentException("Author is required.", nameof(author));
+            }
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                throw new ArgumentException("ISBN is required.", nameof(isbn));
+            }
+
+            if (year < 1450 || year > DateTime.Now.Year + 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year), "Year must be between 1450 and next year.");
+            }
+
+            if (pages <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pages), "Number of pages must be a positive number.");
+            }
+
+            this.Title = title;
+            this.Author = author;
+            this.Year = year;
+            this.Pages = pages;
+            this.Isbn = isbn;
+        }
+    }
+}
--- BookLibrary/Program.cs
+++ BookLibrary/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BookLibrary
+﻿using BookLibrary.Models;
+
+namespace BookLibrary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book = new Book(
+    "Clean Code",
+    "Robert C. Martin",
+    2008,
+    464,
+    "9780132350884");
         }
-    }
+    }  
 }
